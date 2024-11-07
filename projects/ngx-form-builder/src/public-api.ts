ce4d3d8328ebe878f7866ac95f0af6c1848bdcea
Jeffrey Voisard <jeffrey.voisard@bfs.admin.ch>
2024-11-07 08:58:43 +0100

    Wipe form builder library

--- projects/ngx-form-builder/src/public-api.ts
+++ projects/ngx-form-builder/src/public-api.ts
@@ -1,13 +1,3 @@
 /*
  * Public API Surface of ngx-form-builder
  */
-
-export * from './lib/form-builder/form-builder.component'
-
-export * from './lib/dynamic-form.service';
-export * from './lib/local.service';
-
-export * from './lib/shared/pipes/multilingual-text.pipe';
-export * from './lib/shared/validators/empty-to-null.validator';
-
-export * from './lib/form-grid/form-grid.component';
